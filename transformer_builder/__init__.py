570d52b1444528151cd9b2b47d0d7cccc1b5f3ed
MrKekovich <mrkekovich.official@gmail.com>
2023-10-13 18:20:18 +0300

    FT: added CI

--- transformer_builder/__init__.py
+++ transformer_builder/__init__.py
@@ -1,4 +1,4 @@
 from .builders.self_attention_block import SelfAttentionBlock
-from .builders.transformer import TransformerBuilder
+from .builders.transformer_builder import TransformerBuilder
 
 __all__ = ["TransformerBuilder", "SelfAttentionBlock"]
--- transformer_builder/builders/self_attention_block.py
+++ transformer_builder/builders/self_attention_block.py
@@ -3,12 +3,12 @@ from typing import Optional, Callable
 
 class SelfAttentionBlock:
     def __init__(
-            self,
-            before: Optional[Callable] = None,
-            k: Optional[Callable] = None,
-            q: Optional[Callable] = None,
-            v: Optional[Callable] = None,
-            after: Optional[Callable] = None,
-            count: int = 1
+        self,
+        before: Optional[Callable] = None,
+        k: Optional[Callable] = None,
+        q: Optional[Callable] = None,
+        v: Optional[Callable] = None,
+        after: Optional[Callable] = None,
+        count: int = 1,
     ):
         pass
