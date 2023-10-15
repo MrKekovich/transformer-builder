9df16ea6e918287ce4b76b211f6eafcd1f7ea606
MrKekovich <mrkekovich.official@gmail.com>
2023-10-15 20:24:42 +0300

    FT: Expanded Decoder and SelfAttention classes in transformer module

--- transformer_builder/transformer/blocks/attention/self_attention.py
+++ transformer_builder/transformer/blocks/attention/self_attention.py
@@ -1,5 +1,6 @@
-from typing import Optional
+from typing import Optional, Callable
 
+import torch
 from torch import nn
 
 
@@ -10,11 +11,36 @@ class SelfAttention(nn.Module):
         k_architecture: Optional[nn.Module] = None,
         q_architecture: Optional[nn.Module] = None,
         v_architecture: Optional[nn.Module] = None,
+        scale: Optional[Callable[[torch.Tensor], torch.Tensor]] = None,
         layer_after: Optional[nn.Module] = None,
     ):
         super().__init__()
         self.layer_before = layer_before
+
         self.k_architecture = k_architecture
         self.q_architecture = q_architecture
         self.v_architecture = v_architecture
+
+        self.scale = scale or (lambda x: x)  # TODO: add real default scale function
+
         self.layer_after = layer_after
+
+    def forward(
+        self,
+        x: torch.Tensor,
+    ) -> torch.Tensor:
+        x = self.layer_before(x)
+
+        k = self.k_architecture(x)
+        q = self.q_architecture(x)
+        self.v_architecture(x)
+
+        attention_score = (q @ k) / torch.sqrt(
+            torch.tensor(q.size(-1), dtype=torch.float32)
+        )
+
+        attention_score = self.scale(attention_score)
+        attention_score = torch.softmax(attention_score, dim=-1)
+
+        attention = self.layer_after(attention_score)
+        return attention
