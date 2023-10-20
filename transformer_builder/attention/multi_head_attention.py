33593827dc43aa046bb6346bf143485f799d3286
MrKekovich <mrkekovich.official@gmail.com>
2023-10-20 21:23:54 +0300

    FT: Implemented multi_head_attention and wrote

--- transformer_builder/attention/multi_head_attention.py
+++ transformer_builder/attention/multi_head_attention.py
@@ -1,5 +1,30 @@
+import torch
 from torch import nn
 
 
 class MultiHeadAttention(nn.Module):
-    ...
+    def __init__(
+        self,
+        layer_before: nn.Module = nn.Identity(),
+        self_attention_heads: list = None,
+        layer_after: nn.Module = nn.Identity(),
+    ):
+        super().__init__()
+        self.layer_before = layer_before
+        self.self_attention_heads = self_attention_heads or []
+        self.layer_after = layer_after
+
+    def forward(
+        self,
+        x: torch.Tensor,
+        mask: torch.Tensor = None,
+    ) -> torch.Tensor:
+        x = self.layer_before(x)
+        heads_output = [
+            self_attention_head(x, mask)
+            for self_attention_head in self.self_attention_heads
+        ]
+
+        x = torch.concat(heads_output, dim=-1)
+
+        return self.layer_after(x)
