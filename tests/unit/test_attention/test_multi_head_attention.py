49e4ff48597cdf644ff0f39e80e2f7a543d3dfd6
MrKekovich <mrkekovich.official@gmail.com>
2023-10-23 09:08:24 +0300

    TEST: unit tests for multi_head_attention

--- tests/unit/test_attention/test_multi_head_attention.py
+++ tests/unit/test_attention/test_multi_head_attention.py
@@ -1,3 +1,6 @@
+import torch
+from hypothesis import given, settings, strategies as st
+
 from transformer_builder.attention import MultiHeadAttention
 
 
@@ -5,9 +8,18 @@ def test_creation():
     MultiHeadAttention()
 
 
-def test_forward():
-    ...
-
-
-def test_backpropagation():
-    ...
+@given(
+    batch_size=st.integers(min_value=1, max_value=8),
+    sequence_length=st.integers(min_value=1, max_value=8),
+    embedding_dim=st.integers(min_value=1, max_value=10),
+)
+@settings(deadline=None)
+def test_forward(
+    batch_size: int,
+    sequence_length: int,
+    embedding_dim: int,
+):
+    input_tensor = torch.rand(batch_size, sequence_length, embedding_dim)
+    mult_head_attention = MultiHeadAttention()
+    output = mult_head_attention(input_tensor)
+    assert output.shape == input_tensor.shape
