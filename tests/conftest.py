8352ae83567e52a732be035c98830c953bb42e52
MrKekovich <mrkekovich.official@gmail.com>
2023-10-24 15:15:47 +0300

    DOCS: Added docstrings and refactored ResidualConnection class

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,7 +1,29 @@
 import pytest
 import torch
+from torch import nn
 
 
 @pytest.fixture(scope="function")
 def manual_seed(seed: int = 42):
     torch.manual_seed(seed)
+
+
+def backpropagation(
+    model: nn.Module,
+    input_tensor: torch.Tensor,
+    loss_fn: nn.Module = None,
+    optimizer: nn.Module = None,
+) -> None:
+    loss_fn = loss_fn or nn.MSELoss()
+    parameters = list(model.parameters())
+
+    if not optimizer and not parameters:
+        return
+
+    if not optimizer:
+        optimizer = torch.optim.Adam(parameters)
+
+    optimizer.zero_grad()
+    loss = loss_fn(model(input_tensor), input_tensor)
+    loss.backward()
+    optimizer.step()
--- transformer_builder/layers/residual_connection.py
+++ transformer_builder/layers/residual_connection.py
@@ -3,16 +3,29 @@ from torch import nn
 
 
 class ResidualConnection(nn.Module):
+    """
+    Wraps a module with residual connection.
+
+    Remembers input, adds output of the module and applies normalization.
+    """
+
     def __init__(
         self,
-        layer_norm_dimension: int,
         module: nn.Module,
+        normalization: nn.Module,
     ):
+        """
+        Args:
+            module: The module to apply the residual connection to.
+
+            normalization: The normalization module to be applied to
+            the sum of the initial input and the output of the module.
+        """
         super().__init__()
-        self.layer_norm = nn.LayerNorm(layer_norm_dimension)
+        self.normalization = normalization
         self.module = module
 
     def forward(self, x: torch.Tensor, *args, **kwargs):
         _x = x
         x = self.module(x, *args, **kwargs)
-        return self.layer_norm(x + _x)
+        return self.normalization(x + _x)
